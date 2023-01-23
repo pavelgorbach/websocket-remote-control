24dca7a0e140ddcecfefe25b80dd6ec826d55e96
Pavel Gorbach <polgorbach@gmail.com>
2023-01-23 20:49:09 +0300

    feat: add cursor handlers

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { WebSocketServer, createWebSocketStream } from 'ws'
-// import { mouse } from '@nut-tree/nut-js'
+import { mouse } from '@nut-tree/nut-js'
 
 import { httpServer } from './src/http_server'
 
@@ -14,10 +14,24 @@ const wss = new WebSocketServer({ port: 8080 }, () => {
 wss.on('connection', (client, req) => {
   console.log(`Websocket connection opened on port:${req.socket.localPort}.`)
 
-  const duplex = createWebSocketStream(client, { encoding: 'utf8' })
+  const duplex = createWebSocketStream(client, { encoding: 'utf8', decodeStrings: false })
 
-  duplex.on('data', (data) => {
-    console.log('received data', data)
+  duplex.on('data', async (data) => {
+    const [name, arg1, arg2] = data.split(' ')
+
+    duplex.write(`${name} ${arg1} ${arg2}`)
+
+    const { x, y } = await mouse.getPosition()
+
+    if (name === 'mouse_up') {
+      await mouse.setPosition({ x, y: y - arg1 })
+    } else if (name === 'mouse_right') {
+      await mouse.setPosition({ x: x - -arg1, y })
+    } else if (name === 'mouse_down') {
+      await mouse.setPosition({ x, y: y - -arg1 })
+    } else if (name === 'mouse_left') {
+      await mouse.setPosition({ x: x - arg1, y })
+    }
   })
 
   client.on('close', () => {
